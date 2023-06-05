1cb3548b14b05953f4243b51f6d841d6c3238063
Nevra Nur Doğan <122152162+NevraNurDogan@users.noreply.github.com>
2023-06-05 14:40:34 +0300

    Add files via upload

---
+++ BinarySearchTree.java
@@ -0,0 +1,41 @@
+public class BinarySearchTree {
+    /*7, 5, 1, 8, 3, 6, 0, 9, 4, 2] dizisinin Binary-Search-Tree aşamalarını yazınız.
+    Örnek: root x'dir. root'un sağından y bulunur. Solunda z bulunur vb.
+    Verilen dizinin Binary Search Tree (BST) oluşturma aşamaları şu şekildedir:
+
+Adım 1: 7 -> Root düğüm olarak belirlenir.
+
+Adım 2: 5 -> 7'nin soluna yerleştirilir.
+
+Adım 3: 1 -> 5'in soluna yerleştirilir.
+
+Adım 4: 8 -> 7'nin sağına yerleştirilir.
+
+Adım 5: 3 -> 5'in sağına yerleştirilir.
+
+Adım 6: 6 -> 3'ün sağına yerleştirilir.
+
+Adım 7: 0 -> 1'in soluna yerleştirilir.
+
+Adım 8: 9 -> 8'in sağına yerleştirilir.
+
+Adım 9: 4 -> 3'ün sağına yerleştirilir.
+
+Adım 10: 2 -> 1'in sağına yerleştirilir.
+
+Binary Search Tree (BST) oluşturulduktan sonra aşağıdaki şekilde gösterilebilir:
+
+```
+          7
+        /   \
+       5     8
+      / \     \
+     1   6     9
+      \
+       3
+        \
+         0
+          \
+           2
+```*/
+}
