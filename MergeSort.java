019096920986be9b96e8fcabaeed2038be59ee8e
Nevra Nur Doğan <122152162+NevraNurDogan@users.noreply.github.com>
2023-06-05 14:36:56 +0300

    Add files via upload

---
+++ MergeSort.java
@@ -0,0 +1,23 @@
+public class MergeSort {
+    /*[16,21,11,8,12,22] -> Merge Sort
+    Verilen dizinin Merge Sort algoritmasıyla sıralanma aşamaları:
+
+Adım 1: [16, 21, 11, 8, 12, 22]
+Adım 2: [16, 21, 11] [8, 12, 22]
+Adım 3: [16] [21] [11] [8] [12] [22]
+Adım 4: [16, 21] [11] [8, 12] [22]
+Adım 5: [11, 16, 21] [8, 12, 22]
+Adım 6: [8, 11, 12, 16, 21, 22]
+
+Verilen dizi, Merge Sort algoritmasıyla yukarıda gösterilen adımlarla sıralanmıştır.
+
+Big-O gösterimi:
+Merge Sort algoritmasının Big-O gösterimi O(n log n)'dir. Bu, ortalama ve en kötü durumlarda algoritmanın n log n adımda çalışacağı anlamına gelir,
+burada n dizinin eleman sayısıdır. Merge Sort algoritması veriyi ikiye böler, ardından birleştirirken log n adımda çalışır.
+Her bir adımda n elemanını birleştirdiği için n log n adımda tamamlanır.
+
+
+
+
+*/
+}
